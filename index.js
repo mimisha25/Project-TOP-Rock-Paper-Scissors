582e10ca91235d56c06a3312873194d4585b9f7a
mimisha25 <miacorporation25@gmail.com>
2024-03-10 17:19:23 -0400

    Modified functions of computer choice.

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 let plScore = document.querySelector(".your-score");
 let coScore = document.querySelector(".computer-score");
-let round = document.querySelector(".round");
 const result = document.querySelector(".result");
 const div = document.querySelector(".buttons");
 let playerCard = 0;
 let computerCard = 0;
-let roundCard = 0;
-let coSelection=["rock", "paper", "scissor"];
-let computerSelection =coSelection[Math.floor(Math.random()*coSelection.length)];
+let playerSelection;
 
+let coSelection = ["rock", "paper", "scissor"] ;
 
 const divF = e => {
-    let playerSelection = e.target.nodeName === 'BUTTON';
-    if(e.target.id === "rock" && computerSelection === "rock" ||
-             e.target.id === "paper" && computerSelection === "paper" ||
-             e.target.id === "scissor" && computerSelection === "scissor" ){
-             result.innerHTML="win win";
-    }else if(e.target.id === "rock" && computerSelection === "scissor" || 
-             e.target.id === "scissor" && computerSelection === "paper" ||
-             e.target.id === "paper" && computerSelection === "rock"){
-             result.innerHTML="You win";
-             playerCard++;
-    }else if(e.target.id === "rock" && computerSelection === "paper" || 
-             e.target.id === "scissor" && computerSelection === "rock" ||
-             e.target.id === "paper" && computerSelection === "scissor"){
-             result.innerHTML="You'have lost"
-             computerCard++;
+    e.target.nodeName === 'BUTTON';
+    let playerSelection = e.target.id;
+    let computerSelection = coSelection[Math.floor(Math.random() * 3)];
+    if(playerSelection === "rock" && computerSelection === "rock" ||
+    playerSelection === "paper" && computerSelection === "paper" ||
+    playerSelection === "scissor" && computerSelection === "scissor" ){
+    }else if(playerSelection === "rock" && computerSelection === "scissor" || 
+    playerSelection === "scissor" && computerSelection === "paper" ||
+    playerSelection === "paper" && computerSelection === "rock"){
+    playerCard++;
+    }else if(playerSelection === "rock" && computerSelection === "paper" || 
+    playerSelection === "scissor" && computerSelection === "rock" ||
+    playerSelection === "paper" && computerSelection === "scissor"){
+    computerCard++;
+}
+if(computerCard == 10) {
+alert("You lost! Better luck next time :(");
+reset();
+} else if (playerCard == 10) {
+alert("You won! Congratulations! :)")
+reset();
 }
     plScore.innerHTML=playerCard;
     coScore.innerHTML=computerCard;
-}
+    result.innerHTML=`Your choice ${playerSelection}. Computer choice ${computerSelection}`
+    }
 div.addEventListener("click", divF);
+    
+
+    function reset() { 
+        playerCard = 0;
+        computerCard = 0;
+        plScore.innerHTML = "0";
+        coScore.innerHTML = "0";
+        result.innerHTML = "";
+    }
+    
